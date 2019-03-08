0fbf63df69f53f955e7ecb763b355c41637352e9
Zarkoix <adam.towers77@gmail.com>
2019-03-08 10:10:30 -0800

    Updates

---
+++ precache-manifest.f8c41df8ac7c0f5d73318451e22db92c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "b79ae1488e6bee391417",
+    "url": "/static/js/main.8dd89cd2.chunk.js"
+  },
+  {
+    "revision": "c5ada9d28cf99a50a2ee",
+    "url": "/static/js/2.348249c1.chunk.js"
+  },
+  {
+    "revision": "b79ae1488e6bee391417",
+    "url": "/static/css/main.0e8d6779.chunk.css"
+  },
+  {
+    "revision": "d1d99a1edb2e8a551db4750198ad3d02",
+    "url": "/index.html"
+  }
+];
